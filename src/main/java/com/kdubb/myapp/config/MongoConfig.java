1ed1ff9a583f430cab288b1fcdb4ded6439eb877
Erich Reich <ereich@eyereturn.com>
2015-10-30 09:41:41 -0200

    Updated logging, using YML for properties

--- src/main/java/com/kdubb/myapp/config/MongoConfig.java
+++ src/main/java/com/kdubb/myapp/config/MongoConfig.java
@@ -4,85 +4,70 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import com.kdubb.myapp.ConfigConstants;
+import com.kdubb.myapp.config.setting.MongoSettings;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 
 //@Configuration
-@EnableMongoRepositories
+// @EnableMongoRepositories("com.kdubb.myapp.repo")
 public class MongoConfig extends AbstractMongoConfiguration {
 
-	@Value(ConfigConstants.MONGO_DB_HOST)
-	private String mongoDbHost;
-	
-	@Value(ConfigConstants.MONGO_DB_PORT)
-	private int mongoDbPort;
-	
-	@Value(ConfigConstants.MONGO_DB_NAME)
-	private String mongoDbName;
-	
-	@Value(ConfigConstants.MONGO_DB_USERNAME)
-	private String mongoDbUsername;
-	
-	@Value(ConfigConstants.MONGO_DB_PASSWORD)
-	private String mongoDbPassword;
-	
-	@Value(ConfigConstants.MONGO_DB2_HOST)
-	private String mongoDb2Host;
-	
-	@Value(ConfigConstants.MONGO_DB2_PORT)
-	private Integer mongoDb2Port;
-	
-	@Value(ConfigConstants.MONGO_DB2_NAME)
-	private String mongoDb2Name;
-	
-	@Value(ConfigConstants.MONGO_DB2_USERNAME)
-	private String mongoDb2Username;
-	
-	@Value(ConfigConstants.MONGO_DB2_PASSWORD)
-	private String mongoDb2Password;
-	
 	@Override
 	protected String getDatabaseName() {
-		return mongoDbName;
+		return mongoSettings().getName();
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix = "mongo.db")
+	public MongoSettings mongoSettings() {
+		return new MongoSettings();
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix = "mongo.db2")
+	public MongoSettings mongoSettings2() {
+		return new MongoSettings();
 	}
 
 	@Override
 	public Mongo mongo() throws Exception {
-		List<ServerAddress> servers = new ArrayList<ServerAddress>();
-		List<MongoCredential> credentials = new ArrayList<MongoCredential>();
-		
+		List<ServerAddress> servers = new ArrayList<>();
+		List<MongoCredential> credentials = new ArrayList<>();
+
+		MongoSettings settings = mongoSettings();
+		MongoSettings settings2 = mongoSettings2();
+
 		{
-			ServerAddress server = new ServerAddress(mongoDbHost, mongoDbPort);
+			ServerAddress server = new ServerAddress(settings.getHost(), settings.getPort());
 			servers.add(server);
-			
-			MongoCredential credential = MongoCredential.createMongoCRCredential(mongoDbUsername, mongoDbName, mongoDbPassword.toCharArray());
+
+			MongoCredential credential = MongoCredential.createMongoCRCredential(settings.getUsername(), settings.getName(),
+					settings.getPassword().toCharArray());
 			credentials.add(credential);
 		}
-		
-		if(StringUtils.isNotBlank(mongoDb2Host)) {
-			ServerAddress server = new ServerAddress(mongoDb2Host, mongoDb2Port);
+
+		if (StringUtils.isNotBlank(settings2.getHost())) {
+			ServerAddress server = new ServerAddress(settings2.getHost(), settings2.getPort());
 			servers.add(server);
-			
-			MongoCredential credential = MongoCredential.createMongoCRCredential(mongoDb2Username, mongoDb2Name, mongoDb2Password.toCharArray());
+
+			MongoCredential credential = MongoCredential.createMongoCRCredential(settings2.getUsername(), settings2.getName(),
+					settings2.getPassword().toCharArray());
 			credentials.add(credential);
 		}
-		
+
 		return new MongoClient(servers, credentials);
 	}
-	
+
 	@Override
 	@Bean
 	public MongoTemplate mongoTemplate() throws Exception {
-		return new MongoTemplate(mongo(), mongoDbName);
+		return new MongoTemplate(mongo(), mongoSettings().getName());
 	}
 }
---
+++ src/main/java/com/kdubb/myapp/config/setting/MongoSettings.java
@@ -0,0 +1,50 @@
+package com.kdubb.myapp.config.setting;
+
+public class MongoSettings {
+
+	private String host;
+	private Integer port;
+	private String name;
+	private String username;
+	private String password;
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public Integer getPort() {
+		return port;
+	}
+
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
